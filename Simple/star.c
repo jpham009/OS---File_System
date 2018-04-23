1ea8fa3553af63fea2b0a0eaac2fe1a35bf5156f
Johnny Pham <johnny.pham2@mavs.uta.edu>
2018-04-23 03:16:34 -0500

    opening

--- Simple/star.c
+++ Simple/star.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include "functions.h"
+#include "star.h"
 
 int main(){
-	int choice;
-	Disk disk = NULL;
+	int selection;
 
 	while(true){
 		printf("c - create a new repository\n");
@@ -12,64 +11,18 @@ int main(){
 		printf("t - trash a file\n");
 		printf("l - list directory\n");
 		printf("g - get a copy of the file (don't remove)\n");
-		printf("9. Exit\n");
+		printf("q - Exit\n");
 		scanf("%d", &choice);
 
-		switch(choice){
-			case 'c':
-				if(diskIsNull(disk)){
-				disk = allocateDisk();
-				}else{
-					freeDisk(disk);
-					disk = allocateDisk();
-				} 
-				break;
-			case 'a':
-				if(diskIsNull(disk)){
-					printf("Please allocate a disk\n");
-				}else{
-					formatDisk(disk);
-				} 
-				break;
-			case 'd':
-				if(diskIsNull(disk)){
-					printf("Please allocate a disk\n");
-				}else{
-					addFile(disk);
-				} 
-				break;
-			case 't':
-				if (diskIsNull(disk))
-				{
-					printf("Please allocate a disk\n");
-				}else{
-					extractFile(disk);
-				}
-				break;
-			case 'l':
-				if(diskIsNull(disk)){
-					printf("Please allocate a disk\n");
-				}else{
-					printDiskInformation(disk);
-				} 
-				break;
-			case 'g':
-				if(diskIsNull(disk)){
-					printf("Please allocate a disk\n");
-				}else{
-					printDiskToFile(disk);
-				} 
-				break;
-			case 'q':
-				if(diskIsNull(disk)){
-					printf("Please allocate a disk\n");
-				}else{
-					renameFile(disk);
-				} 
-				break;
-		
-			default:
-				printf("Not a valid choice\n");
+		switch(selection){
+			case 'c': createRepository(); break; 
+			case 'a': addFile(); break;
+			case 'd': deleteFile(); break;
+			case 't': trashFile(); break;
+			case 'l': listDirectory(); break;
+			case 'g': copyFile(); break;
+			case 'q': exit(0); break;		
+			default: printf("Not a valid choice\n");
 
 		}
 	}
